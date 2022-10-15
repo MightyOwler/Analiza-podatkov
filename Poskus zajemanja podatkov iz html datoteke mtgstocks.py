e805ca56e415cb81a4c978c87b06951f8c97bb5b
MightyOwler <jasa.knap@gmail.com>
2022-10-15 21:38:34 +0200

    Delujoče zajemanje podatkov setov s spletne strani

--- Poskus zajemanja podatkov iz html datoteke mtgstocks.py
+++ Poskus zajemanja podatkov iz html datoteke mtgstocks.py
@@ -2,11 +2,23 @@ import re
 import requests
 import csv
 import json
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
+
+
+
+# Tole dvoje je treba, da se Chrome sproti ne odpira
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome("chromedriver.exe", options=chrome_options)
 
 PRVI_SET = 810
 ZADNJI_SET = 812
 
-debug_mode = True
+debug_mode = False
 
 vzorec_bloka = re.compile(
     r'<td><mtg-set-icon.*?'
@@ -41,48 +53,54 @@ def pridobi_ustrezno_ime_lokalne_datoteke(st_strani):
     return f"Podatki o kartah/Karte iz seta st. {st_strani}.html"
 
 
+# Najprej poradiramo csv datoteko
+with open("karte.csv", "w") as dat:
+    pass
 
 
-# for st_strani in range(PRVI_SET, ZADNJI_SET):
-#     url = (
-#         f'https://www.mtgstocks.com/sets/{st_strani}' 
-#     )
-#     print(f"Zajemam {url}")
-#     response = requests.post(url, allow_redirects=False, timeout=5, headers={
-#         # "Accept-Language": "sl-si"
-#     })
-#     vsebina = response.text
-#     with open(pridobi_ustrezno_ime_lokalne_datoteke(st_strani), 'w') as dat:
-#         dat.write(vsebina)
+for st_strani in range(PRVI_SET, ZADNJI_SET):
+    url = (
+        f'https://www.mtgstocks.com/sets/{st_strani}' 
+    )
+    print(f"Zajemam {url}")
+    driver.get(url)
+    driver.find_element(by=By.XPATH, value='//*[@id="overview"]/mtg-sets-overview/data-table/div[2]/div/div/table/thead/tr[1]/th[3]').click()
+    #driver.find_element("xpath", "//input[@value='Market']").click()
+    
+    # response = requests.post(url, allow_redirects=False, timeout=5, headers={
+    #     # "Accept-Language": "sl-si"
+    # })
+    vsebina = driver.page_source
+    with open(pridobi_ustrezno_ime_lokalne_datoteke(st_strani), 'w') as dat:
+        dat.write(vsebina)
 
 
+# Delujoče zajemanje
 
-
-
-for st_seta in range(PRVI_SET, ZADNJI_SET):
-    with open(f"Podatki o kartah/Karte iz seta st. {st_seta}.html", "r") as f:
+# for st_seta in range(PRVI_SET, ZADNJI_SET):
+#     with open(f"Podatki o kartah/Karte iz seta st. {st_seta}.html", "r") as f:
         
-        print(f"Berem set št. {st_seta}")
-        karte = []
-        stetje = 0
+#         print(f"Berem podatke kar iz seta #{st_seta}")
+#         karte = []
+#         stetje = 0
         
-        vsebina = f.read()
-        for blok in vzorec_bloka.finditer(vsebina):
-            stetje += 1
+#         vsebina = f.read()
+#         for blok in vzorec_bloka.finditer(vsebina):
+#             stetje += 1
             
-            if debug_mode:
-                print(stetje)
-                print(blok.group(0))
-                print(vzorec_karte.search(blok.group(0)))
+#             if debug_mode:
+#                 print(stetje)
+#                 print(blok.group(0))
             
-            karte.append(izloci_podatke_o_kartah(blok.group(0)))
+#             karte.append(izloci_podatke_o_kartah(blok.group(0)))
+#             time.sleep(5)
             
 
     with open("karte.json", "w") as dat:
         json.dump(karte, dat, indent=4, ensure_ascii=False)
         
 
-    with open("karte.csv", "w") as dat:
+    with open("karte.csv", "a") as dat:
         writer = csv.DictWriter(dat, [
             "id_karte",
             "ime",
